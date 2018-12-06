eb9705e187509cc548dea0080cfcb285f9452342
GiorgioPingendo <giorgio.seregni@pingendo.com>
2018-12-06 15:05:24 +0100

    more

--- pingendocontrib/contrib.js
+++
@@ -1,5 +0,0 @@
-if(window.location.hash && window.location.hash.split("=")[0] == "#invite_token")
-	location = "/pingendocontrib/complete_signup?token="+window.location.hash.split("=")[1];
-
-if(window.location.hash && window.location.hash.split("=")[0] == "#recovery_token")
-	location = "/pingendocontrib/recovery_password?token="+window.location.hash.split("=")[1];
